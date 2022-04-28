5e93189d155cfaa652e5e75f5956e2392b440145
GiriRaj249 <80571820+GiriRaj249@users.noreply.github.com>
2022-04-28 22:15:47 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,8 +15,8 @@ CONFIG="--config-path=$GITHUB_WORKSPACE/action/.gitleaks.toml"
 
 if [ "$GITHUB_EVENT_NAME" = "push" ]
 then
-  echo gitleaks --path=$GITHUB_WORKSPACE --verbose --redact  $CONFIG
-  CAPTURE_OUTPUT=$(gitleaks --path=$GITHUB_WORKSPACE --verbose --redact  $CONFIG)
+  echo gitleaks --path=$GITHUB_WORKSPACE --verbose --redact --commit=$GITHUB_SHA $CONFIG
+  CAPTURE_OUTPUT=$(gitleaks --path=$GITHUB_WORKSPACE --verbose --redact --commit=$GITHUB_SHA $CONFIG)
 elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]
 then 
   git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" remotes/origin/$GITHUB_BASE_REF... > commit_list.txt
